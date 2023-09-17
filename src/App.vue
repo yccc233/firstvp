ba924191fdb1eedcd48b9641ca1abc072cc78f75
俞澄 <1099312164@qq.com>
2023-09-17 15:24:42 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+// 定义响应性引用
+const msg = ref('这是一条消息');
+const rawHtml = ref('<strong>这是一些HTML内容</strong>');
+</script>
+
+<template>
+  <span>Message: {{ msg }}</span>
+
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+</template>
+
+<style scoped></style>
